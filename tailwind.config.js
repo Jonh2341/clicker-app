66029e15eed217315b53ca724f49a0eb80633206
Adam <rokakaak@Gmail.com>
2025-01-14 19:41:08 +0200

    mui tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",  // Збиратиме всі HTML та JS файли в папці src
+    "./public/index.html" // Також аналізуватиме цей конкретний файл
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
